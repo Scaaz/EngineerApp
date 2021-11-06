d8233dada40525781487efa8799c4aabfe613876
Szymon Czekaj <szymon.czekaj@pirios.com>
2021-11-06 16:26:37 +0100

    Axios posts to backend

--- EngineerApp/ClientApp/src/components/Home.vue
+++ EngineerApp/ClientApp/src/components/Home.vue
@@ -1,41 +1,55 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <img src="../assets/logo.png" />
-        <p>
-           Chad praca in¿ynierka, ju¿ niemal¿e gotowa
-        </p>
-        <p>
-            Tutaj bêdzie solidny opis co wgl ta apka robi
-        </p>
-        <h3>Tutaj bêdzie pole do wrzucenia pliku</h3>
-        
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label
+        >File
+        <input type="file" id="file" ref="myFiles" @change="handleFileUpload()" />
+      </label>
+      <button @click="submitFile()">Submit</button>
     </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'Home',
-  props: {
-    msg: String
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      file: "",
+    };
+  },
+
+  methods: {
+    handleFileUpload() {
+      console.log('start handle')
+       this.file = this.$refs.myFiles.files
+      console.log(this.file)
+      console.log('end handle')
+    },
+
+    submitFile() {
+      let formData = new FormData();
+
+      formData.append("file", this.file);
+
+      axios
+        .post("api/UploadFile", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(function() {
+          console.log("SUCCESS!!");
+        })
+        .catch(function() {
+          console.log("FAILURE!!");
+        });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
---
+++ EngineerApp/Controllers/fileUploadController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace EngineerApp.Controllers
+{
+    public class fileUploadController : Controller
+    {
+        [HttpPost]
+        [Route("api/UploadFile", Name = "UploadFile")]
+        public async void UploadFile(IFormCollection Upload)
+        {
+           
+            if (Upload.Files.Count > 0)
+            {
+                
+                //If we have at least one file in the IFormCollection, then perform whatever storage actions
+            }
+        }
+    }
+}
